6c699f788ef20d8600a12293e226a9d5f88dc07d
kardelenssu <kurtkardelensu@gmail.com>
2024-08-22 21:08:00 +0200

    first commit

---
+++ GitDenemeClass.java
@@ -0,0 +1,9 @@
+package GitDeneme;
+
+public class GitDenemeClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("Task 1 Kodlari");
+        System.out.println(" Task 2 Kodlari");
+    }
+}
